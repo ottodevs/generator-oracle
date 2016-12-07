ab4483fdba695a35f30bd18925c19c0af05a0e55
A92hm <ali92hm@gmail.com>
2016-12-06 20:19:00 -0800

    added base functions

--- src/generators/app/index.js
+++ src/generators/app/index.js
@@ -1,5 +1,74 @@
+import { Base } from 'yeoman-generator'
 
-import path from 'path'
-// import fs from 'fs'
+export class Generator extends Base {
+  constructor (...args) {
+    super(...args)
 
-console.log(path)
+    this.option('skip-install', {
+      desc: 'Do not install dependencies',
+      type: Boolean,
+      defaults: false
+    })
+
+    this.option('skip-config', {
+      desc: 'Always use existing .yo-rc.json',
+      type: Boolean,
+      defaults: false
+    })
+  }
+
+  get initializing () {
+    return {
+      init () {
+        console.log(`The name is:`)
+      }
+    }
+  }
+
+  get prompting () {
+    return {
+    }
+  }
+
+  get configuring () {
+    function init () {
+      console.log('init')
+    }
+
+    function dothings () {
+      console.log('init')
+    }
+
+    function finish () {
+      console.log('init')
+    }
+
+    return {
+      init,
+      dothings,
+      finish
+    }
+  }
+
+  get default () {
+    return {}
+  }
+
+  get conflicts () {
+    return {}
+  }
+
+  get writing () {
+    return {}
+  }
+
+  get install () {
+    return {}
+  }
+
+  get end () {
+    return {}
+  }
+}
+
+module.exports = Generator
